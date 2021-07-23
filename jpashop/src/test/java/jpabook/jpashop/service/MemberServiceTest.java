2bd2c24ea88dff86cd3b0099f3031ef516c75f2a
hansolShin <hansolstar@naver.com>
2021-07-24 00:08:58 +0900

    MemberService 개발

--- jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,7 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;import static org.junit.Assert.assertEquals;
+import org.springframework.transaction.annotation.Transactional;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 @RunWith(SpringRunner.class)
